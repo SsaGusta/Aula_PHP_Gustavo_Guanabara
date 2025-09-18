431a4e69b360261f675c9cb53c9e732c2df6f49a
SsaGusta <gustavoliveiragf15@gmail.com>
2025-09-18 14:29:23 -0300

    feat: Criando o exemplo do segundo desafio

---
+++ Desafios/Desafio2/Des2.php
@@ -0,0 +1,10 @@
+<?php 
+    $min = 0;
+    $max = 100;
+
+    $num = mt_rand($min, $max);
+    echo "Gerando um número aleatório entre $min e $max... \n";
+    echo "O número gerado foi <strong>$num</strong>";
+
+
+?>
