5fa4e133b726bcfea0fa97e53838ecb63c4b43ba
SsaGusta <gustavoliveiragf15@gmail.com>
2025-09-18 14:06:36 -0300

    feat: Organizando arquivos em pastas

---
+++ Desafios/Desafio1/Des1.php
@@ -0,0 +1,10 @@
+<?php 
+    $num = 15;
+    $antessesor = $num - 1;
+    $sucessor = $num + 1;
+
+    echo "O número é " . $num . " e seu antecessor é " . $antessesor . "\n"; 
+    echo "O número é " . $num . " e seu sucessor é " . $sucessor . "\n";
+
+
+?>
