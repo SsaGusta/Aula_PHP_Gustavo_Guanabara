a1b6d75deab208f20d4ca0d3e403d963b5231827
SsaGusta <gustavoliveiragf15@gmail.com>
2025-09-23 14:34:18 -0300

    feat: exercicio 5, como funciona as super globais

---
+++ Exercicios/ex005/superglobais.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>Exercicio SuperGlobais</title>
+</head>
+<body>
+    <main>
+        <pre>
+            <?php
+            echo "<h1>SuperGlobais GET</h1>";
+
+            ?>
+        </pre>
+    </main>
+    
+</body>
+</html>
